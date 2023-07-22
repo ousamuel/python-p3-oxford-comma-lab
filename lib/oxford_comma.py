1586b06a2ca0591a5ef633f354b4c67b17cc93d4
Samuel <samuelou510@gmail.com>
2023-07-22 19:00:49 -0400

    done, learned about array splicing format array[start, end, step]

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
+
 def oxford_comma(items):
+    if len(items) == 1:
+        return items[0];
+    elif len(items) == 2:
+        return (" and ").join(items)
+    elif len(items) == 3:
+        return ", ".join(items[:-1]) + ", and " + items[-1]
+    elif len(items) > 3:
+        return  ", ".join(items[:-1]) + ", and " + items[-1]
     return None
